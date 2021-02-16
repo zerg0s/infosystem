856e6f94a9d3067bebbf66f9aaeec858cda348e9
zerg0s <pul_forever@mail.ru>
2021-02-16 15:56:46 +0300

    Add files via upload

---
+++ RunInformationSystem.bat
@@ -0,0 +1,8 @@
+cd %~dp0
+chcp 1251
+IF EXIST "InformationSystem.jar_new.jar" (
+  del /f InformationSystem.jar
+  rename InformationSystem.jar_new.jar InformationSystem.jar
+)
+
+java.exe --module-path "%cd%\lib" --add-modules javafx.controls,javafx.fxml  -Dfile.encoding=UTF-8 -classpath "InformationSystem.jar;.\lib\javafx-swt.jar;.\lib\javafx.web.jar;.\lib\javafx.base.jar;.\lib\javafx.fxml.jar;.\lib\javafx.media.jar;.\lib\javafx.swing.jar;.\lib\javafx.controls.jar;.\lib\javafx.graphics.jar" informationsystem.InformationSystem
