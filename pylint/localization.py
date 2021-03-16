9c57e49834c85e396b07c65ced97a6e4ba872664
zerg0s <pul_forever@mail.ru>
2021-03-16 16:25:11 +0300

    Update localization.py

--- pylint/localization.py
+++ pylint/localization.py
@@ -1,4 +1,6 @@
 class Locale:
+    RecievedAnswer = "Recieved answer: "
+    Total = "Finally: "
     DoesntHaveMandatory = "Source code may not contain mandatory values or functions. Read the task more carefully! Please retry."
     NotInFunctional = "Source code is not in functional style. Please retry. Only one language operator is allowed."
     HaveRestricted = "Source code may contain restricted values or functions. Please retry."
@@ -9,4 +11,4 @@ class Locale:
     EasyModeHelp = "Wrong answer was received for input:\n%s"
     CrashFound = "\nProgram crash found in your reply:\n"
     YourAnswer = "\nYour answer is:\n"
-
+    Test = "Test"
