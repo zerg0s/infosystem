cbfa9465d7720143ce2a5caa24899446066372e8
Politsyn <Politsyn@politsyn7.avp.ru>
2021-03-17 11:56:57 +0300

    some refactoring

--- src/redmineManagement/RedmineAlternativeReader.java
+++ src/redmineManagement/RedmineAlternativeReader.java
@@ -15,7 +15,9 @@ import okhttp3.HttpUrl;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
-import org.json.*;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 /**
  * @author user
