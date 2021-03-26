3a46b1f72bb2a77071723dcbffa2638378d76396
Politsyn <Politsyn@politsyn7.avp.ru>
2021-03-26 12:04:51 +0300

    bf

--- JavaDir/localization.py
+++ JavaDir/localization.py
@@ -1,4 +1,6 @@
 class Locale:
+    ReceivedAnswer = "Received answer: "
+    Total = "Finally: "
     DoesntHaveMandatory = "Source code may not contain mandatory values or functions. Read the task more carefully! Please retry."
     NotInFunctional = "Source code is not in functional style. Please retry. Only one language operator is allowed."
     HaveRestricted = "Source code may contain restricted values or functions. Please retry."
@@ -8,5 +10,5 @@ class Locale:
     Passed = "Passed"
     EasyModeHelp = "Wrong answer was received for input:\n%s"
     CrashFound = "\nProgram crash found in your reply:\n"
-    YourAnswer = "\nYour answer is:\n"
-
+    YourAnswer = "\nYour answer is: \n"
+    Test = "Test"
