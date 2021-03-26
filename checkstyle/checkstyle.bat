1a3b380df61b5743a653781b9366526f7445608d
Politsyn <Politsyn@politsyn7.avp.ru>
2021-03-26 12:05:43 +0300

    add checkstyle 8.41

--- checkstyle/checkstyle.bat
+++ checkstyle/checkstyle.bat
@@ -2,5 +2,5 @@
 cd %~dp0
 cd ..
 set JAVA_FILE=%1%
-java -jar checkstyle\checkstyle-8.8-all.jar -c /sun_checks.xml %JAVA_FILE% 1> %JAVA_FILE%_errorReport.txt 2>&1
+java -jar checkstyle\checkstyle-8.41-all.jar -c checkstyle\sun_checks.xml %JAVA_FILE% 1> %JAVA_FILE%_errorReport.txt 2>&1
 rem java -jar checkstyle-8.8-all.jar -c /sun_checks.xml %JAVA_FILE%
